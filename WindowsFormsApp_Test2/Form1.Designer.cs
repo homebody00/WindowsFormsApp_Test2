3c311adc5dac5139a2eb42b182455c570f4d6639
homebody00 <youth1291@gmail.com>
2024-12-17 11:40:26 +0900

    Add project files.

---
+++ WindowsFormsApp_Test2/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFormsApp_Test2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_print = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_print
+            // 
+            this.textBox_print.Location = new System.Drawing.Point(34, 73);
+            this.textBox_print.Multiline = true;
+            this.textBox_print.Name = "textBox_print";
+            this.textBox_print.Size = new System.Drawing.Size(732, 338);
+            this.textBox_print.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("12LotteMartDreamBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(30, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Result: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_print);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_print;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp_Test2/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Test2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] str = new string[10];
+
+            string s1 = "동해 물과 백두산이";
+            string s2 = "토요일에 먹는 토마토";
+            string s3 = "질서 있는 퇴장";
+            string s4 = "그 사람의 그림자는 그랬다.";
+            string s5 = "삼성 갤럭시";
+            string s6 = "오늘은 왠지 더 배고프다";
+            string s7 = "이름,나이,전화번호";
+            string s8 = "우리 나라 만세";
+
+            str[0] = s1.IndexOf("백두산").ToString();
+            str[1] = s2.IndexOf("토").ToString();
+            str[2] = s3.Contains("퇴").ToString();
+            str[3] = s4.Replace("그", "이");
+            str[4] = s5.Insert(3, "애플 ");
+            str[5] = s6.Remove(7,1);
+            String[] temp = s7.Split(',');
+            for (int i = 0; i < temp.Length; i++)
+            {
+                str[6 + i] = temp[i];
+            }
+            str[9] = s8.Substring(3, 2);
+
+            textBox_print.Text = "";
+            for (int i = 0; i < 10; i++)
+            {
+                textBox_print.Text += str[i] + "\r\n";
+            }
+        }
+    }
+}
