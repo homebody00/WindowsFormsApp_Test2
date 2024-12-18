2e88afdad83c1846e0afbbc295f328931e46bf08
homebody00 <youth1291@gmail.com>
2024-12-18 11:45:58 +0900

    실습2. if문

--- WindowsFormsApp_Test2/Practice241218.cs
+++ WindowsFormsApp_Test2/Practice241218.cs
@@ -12,29 +12,66 @@ namespace WindowsFormsApp_Test2
 {
     public partial class Practice241218 : Form
     {
+        #region 실습1. 함수.
+        //public practice241218()
+        //{
+        //    initializecomponent();
+        //    int num1 = 201;
+        //    int num2 = 10;
+
+        //    int[] result = getshareandrest(num1, num2);
+
+        //    textbox_print.text = num1 + " / " + num2 + " = " + result[0] + "\r\n";
+        //    textbox_print.text += num1 + " % " + num2 + " = " + result[1];
+        //}
+
+        //int[] getshareandrest(int a, int b)
+        //{
+        //    int[] intarray = new int[3];
+
+        //    intarray[0] = a / b;
+        //    intarray[1] = a % b;
+
+        //    return intarray;
+        //}
+        #endregion
+
         public Practice241218()
         {
             InitializeComponent();
-            int num1 = 201;
-            int num2 = 10;
 
-            int[] result = getShareAndRest(num1, num2);
+            bool result = throwCoin(false);
 
-            textBox_print.Text = num1 + " / " + num2 + " = " + result[0] + "\r\n";
-            textBox_print.Text += num1 + " % " + num2 + " = " + result[1];
-
-
-
+            textBox_print.Text = result.ToString() + "\r\n";
         }
 
-        int[] getShareAndRest(int a, int b)
+        bool throwCoin(bool a)
         {
-            int[] intArray = new int[3];
+            Random randomObj = new Random();
+            int randomValue = randomObj.Next(); // int형 난수 생성
+            randomValue %= 2;
 
-            intArray[0] = a / b;
-            intArray[1] = a % b;
-
-            return intArray;
+            if (randomValue == 1)
+            {
+                if (a == true)
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else if (randomValue == 0)
+            {
+                if (a == false)
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else
+                return false;
         }
+
     }
 }
