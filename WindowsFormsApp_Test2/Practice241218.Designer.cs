1ab323b0bd26714d1cc38721ebc951bfe7802801
homebody00 <youth1291@gmail.com>
2024-12-19 09:36:30 +0900

    실습. 사용자 입력.

--- WindowsFormsApp_Test2/Practice241218.Designer.cs
+++ WindowsFormsApp_Test2/Practice241218.Designer.cs
@@ -28,36 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox_print = new System.Windows.Forms.TextBox();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.button_input = new System.Windows.Forms.Button();
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.radioButton_True = new System.Windows.Forms.RadioButton();
+            this.radioButton_False = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
-            // label1
+            // textBox_result
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("NanumGothicCoding", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(31, 40);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 27);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Result: ";
+            this.textBox_result.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox_result.Font = new System.Drawing.Font("NanumGothicCoding", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox_result.Location = new System.Drawing.Point(11, 64);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.ReadOnly = true;
+            this.textBox_result.Size = new System.Drawing.Size(374, 338);
+            this.textBox_result.TabIndex = 2;
             // 
-            // textBox_print
+            // button_input
             // 
-            this.textBox_print.Font = new System.Drawing.Font("NanumGothicCoding", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.textBox_print.Location = new System.Drawing.Point(36, 70);
-            this.textBox_print.Multiline = true;
-            this.textBox_print.Name = "textBox_print";
-            this.textBox_print.Size = new System.Drawing.Size(732, 338);
-            this.textBox_print.TabIndex = 4;
+            this.button_input.Font = new System.Drawing.Font("NanumGothicCoding", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_input.Location = new System.Drawing.Point(311, 15);
+            this.button_input.Name = "button_input";
+            this.button_input.Size = new System.Drawing.Size(75, 23);
+            this.button_input.TabIndex = 1;
+            this.button_input.Text = "Input";
+            this.button_input.UseVisualStyleBackColor = true;
+            this.button_input.Click += new System.EventHandler(this.button_input_Click);
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Font = new System.Drawing.Font("NanumGothicCoding", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox_input.Location = new System.Drawing.Point(12, 15);
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(293, 20);
+            this.textBox_input.TabIndex = 0;
+            // 
+            // radioButton_True
+            // 
+            this.radioButton_True.AutoSize = true;
+            this.radioButton_True.Location = new System.Drawing.Point(12, 42);
+            this.radioButton_True.Name = "radioButton_True";
+            this.radioButton_True.Size = new System.Drawing.Size(44, 16);
+            this.radioButton_True.TabIndex = 3;
+            this.radioButton_True.TabStop = true;
+            this.radioButton_True.Text = "true";
+            this.radioButton_True.UseVisualStyleBackColor = true;
+            this.radioButton_True.CheckedChanged += new System.EventHandler(this.radioButton_True_CheckedChanged);
+            // 
+            // radioButton_False
+            // 
+            this.radioButton_False.AutoSize = true;
+            this.radioButton_False.Location = new System.Drawing.Point(67, 42);
+            this.radioButton_False.Name = "radioButton_False";
+            this.radioButton_False.Size = new System.Drawing.Size(50, 16);
+            this.radioButton_False.TabIndex = 3;
+            this.radioButton_False.TabStop = true;
+            this.radioButton_False.Text = "false";
+            this.radioButton_False.UseVisualStyleBackColor = true;
+            this.radioButton_False.CheckedChanged += new System.EventHandler(this.radioButton_False_CheckedChanged);
             // 
             // Practice241218
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox_print);
+            this.ClientSize = new System.Drawing.Size(397, 414);
+            this.Controls.Add(this.radioButton_False);
+            this.Controls.Add(this.radioButton_True);
+            this.Controls.Add(this.textBox_input);
+            this.Controls.Add(this.button_input);
+            this.Controls.Add(this.textBox_result);
             this.Name = "Practice241218";
             this.Text = "Practice241218";
             this.ResumeLayout(false);
@@ -66,8 +107,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox_print;
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.Button button_input;
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.RadioButton radioButton_True;
+        private System.Windows.Forms.RadioButton radioButton_False;
     }
 }
--- WindowsFormsApp_Test2/Practice241218.cs
+++ WindowsFormsApp_Test2/Practice241218.cs
@@ -4,14 +4,26 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WindowsFormsApp_Test2
 {
     public partial class Practice241218 : Form
     {
+        public Practice241218()
+        {
+            InitializeComponent();
+
+
+
+
+        }
+
         #region 실습1. 함수.
         //public practice241218()
         //{
@@ -36,13 +48,96 @@ namespace WindowsFormsApp_Test2
         //}
         #endregion
 
-        public Practice241218()
+        #region 실습2. if문
+        //public Practice241218()
+        //{
+        //    InitializeComponent();
+
+        //    bool result = coinTossGame(false);
+
+        //    textBox_print.Text = result.ToString() + "\r\n";
+        //}
+
+        //bool coinTossGame(bool a)
+        //{
+        //    Random randomObj = new Random();
+        //    int randomValue = randomObj.Next(); // int형 난수 생성
+        //    randomValue %= 2;
+        //    bool coin = randomValue == 1;
+
+        //    if (coin == a)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+
+
+        //}
+        #endregion
+
+        private void button_input_Click(object sender, EventArgs e)
         {
-            InitializeComponent();
+            bool choice;
+            bool gameResult;
 
-            bool result = coinTossGame(false);
+            if (textBox_input.Text.ToLower() == "true")
+            {
+                choice = true;
+                gameResult = coinTossGame(choice);
 
-            textBox_print.Text = result.ToString() + "\r\n";
+                textBox_result.Text = "입력하신 값은 " + choice + " 입니다.\r\n";
+                textBox_result.Text += "동전 던지기 결과...\r\n";
+
+                if (gameResult == true)
+                    textBox_result.Text += "승리!\r\n";
+                else
+                    textBox_result.Text += "패배\r\n";
+            }
+            else if (textBox_input.Text.ToLower() == "false")
+            {
+                choice = false;
+                gameResult = coinTossGame(choice);
+
+                textBox_result.Text = "입력하신 값은 " + choice + " 입니다.\r\n";
+                textBox_result.Text += "동전 던지기 결과...\r\n";
+
+                if (gameResult == true)
+                    textBox_result.Text += "승리!\r\n";
+                else
+                    textBox_result.Text += "패배\r\n";
+            }
+            else 
+                textBox_result.Text = "라디오 버튼을 눌러 입력해주세요.\r\n";
+
+
+            textBox_input.Text = string.Empty;
+        }
+
+        private void radioButton_True_CheckedChanged(object sender, EventArgs e)
+        {
+            if (textBox_input.Text == "" || textBox_input.Text == "false" || textBox_input.Text == "true")
+            {
+                textBox_input.Text = "true";
+            }
+            else
+            {
+                textBox_result.Text = "라디오 버튼을 눌러 입력해주세요.\r\n";
+            }
+        }
+
+        private void radioButton_False_CheckedChanged(object sender, EventArgs e)
+        {
+            if (textBox_input.Text == "" || textBox_input.Text == "false" || textBox_input.Text == "true")
+            {
+                textBox_input.Text = "false";
+            }
+            else
+            {
+                textBox_result.Text = "라디오 버튼을 눌러 입력해주세요.\r\n";
+            }
         }
 
         bool coinTossGame(bool a)
@@ -60,9 +155,6 @@ namespace WindowsFormsApp_Test2
             {
                 return false;
             }
-
-            
         }
-
     }
 }
