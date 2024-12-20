20e35ed111e3b559fb852dc056b1ede29841c1b1
homebody00 <youth1291@gmail.com>
2024-12-20 10:24:53 +0900

    실습. for문

---
+++ WindowsFormsApp_Test2/Practice241220.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApp_Test2
+{
+    partial class Practice241220
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.button_input = new System.Windows.Forms.Button();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("NanumGothicCoding", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(11, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 15);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "학생 수를 입력해주세요.";
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Font = new System.Drawing.Font("NanumGothicCoding", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox_input.Location = new System.Drawing.Point(11, 42);
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(293, 20);
+            this.textBox_input.TabIndex = 9;
+            this.textBox_input.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_input_KeyDown);
+            // 
+            // button_input
+            // 
+            this.button_input.Font = new System.Drawing.Font("NanumGothicCoding", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_input.Location = new System.Drawing.Point(310, 42);
+            this.button_input.Name = "button_input";
+            this.button_input.Size = new System.Drawing.Size(75, 23);
+            this.button_input.TabIndex = 10;
+            this.button_input.Text = "Input";
+            this.button_input.UseVisualStyleBackColor = true;
+            this.button_input.Click += new System.EventHandler(this.button_input_Click);
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox_result.Font = new System.Drawing.Font("NanumGothicCoding", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox_result.Location = new System.Drawing.Point(12, 68);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.ReadOnly = true;
+            this.textBox_result.Size = new System.Drawing.Size(374, 338);
+            this.textBox_result.TabIndex = 11;
+            // 
+            // Practice241220
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 421);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_input);
+            this.Controls.Add(this.button_input);
+            this.Controls.Add(this.textBox_result);
+            this.Name = "Practice241220";
+            this.Text = "Practice241220";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.Button button_input;
+        private System.Windows.Forms.TextBox textBox_result;
+    }
+}
---
+++ WindowsFormsApp_Test2/Practice241220.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Test2
+{
+    public partial class Practice241220 : Form
+    {
+        public Practice241220()
+        {
+            InitializeComponent();
+
+            // 1. 학생 수 입력
+            // 2. 학생 수 만큼 랜덤 점수 생성하고 각 학생에게 할당 (100점 만점)
+            // 3. 학생 이름은 학생1, 학생2, ...
+            // 4. 학생1의 점수: 42점 << 형태로 결과 표시 (함수 생성)
+
+
+        }
+        private void button_input_Click(object sender, EventArgs e)
+        {
+            textBox_result.Text = string.Empty;
+            string str = textBox_input.Text;
+            int numberOfStudents = int.Parse(str);
+            createScore(numberOfStudents);
+        }
+
+        void createScore(int a)
+        {
+            Random randomObj = new Random();
+            for (int i = 1; i <= a; i++)
+            {
+                int randomValue = randomObj.Next(); // int형 난수 생성
+                int score = randomValue % 101;
+
+                string name = "학생" + i.ToString();
+
+                printScore(name, score);
+            }
+        }
+        
+
+        void printScore(string name, int score)
+        {
+            textBox_result.Text += name + "의 점수: " + score + "점 \r\n";
+        }
+
+        private void textBox_input_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button_input_Click(sender, e);
+            }
+        }
+    }
+}
